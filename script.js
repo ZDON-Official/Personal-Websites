51eb8a5522ca8e1adb3520e8d8619bee27901b41
Zohaib <zrana1214@gmail.com>
2023-08-12 22:46:22 -0400

    added dark mode support

---
+++ script.js
@@ -0,0 +1,56 @@
+const darkMode_Toggle = document.getElementById("theme_toggle");
+var darkMode_off = false;
+var darkMode;
+
+if (localStorage.getItem("dark-mode")) {
+  // if dark mode is in storage, set variable with that value
+  darkMode = localStorage.getItem("dark-mode");
+} else {
+  darkMode = "light";
+}
+
+localStorage.setItem("dark-mode", darkMode);
+
+if (localStorage.getItem("dark-mode") == "dark") {
+  // if the above is 'dark' then apply .dark to the body
+  // document.body.classList.add("dark");
+
+  document.documentElement.style.setProperty("--BG", "rgb(33, 36, 53)");
+  document.documentElement.style.setProperty("--dark_shades", "#05383f");
+
+  //! hide the 'dark' button
+  // $('.dark-button').hide();
+
+  //! show the 'light' button
+  // $('.light-button').show();
+}
+
+// Toggle dark UI
+
+darkMode_Toggle.addEventListener("click", () => {
+  console.log("theme btn pressed");
+
+  if (darkMode_off === false) {
+    // console.log("dark mode on");
+    darkMode_off = true;
+
+    document.documentElement.style.setProperty("--background_color", "#121026");
+    document.documentElement.style.setProperty("--secondary_color", "#178582");
+
+    // document.body.classList.add("dark");
+    localStorage.setItem("dark-mode", "dark");
+  } else {
+    // console.log("dark mode off");
+
+    document.documentElement.style.setProperty("--background_color", "#e9eef1");
+    document.documentElement.style.setProperty(
+      "--secondary_color",
+      "#142727df"
+    );
+
+    // document.body.classList.remove("dark");
+    localStorage.setItem("dark-mode", "light");
+
+    darkMode_off = false;
+  }
+});
