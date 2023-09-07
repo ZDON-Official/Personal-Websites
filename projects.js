22b0ca10701414291e40d6a1b36b876635961871
Zohaib <zrana1214@gmail.com>
2023-09-07 08:44:09 -0400

    project page auto fill added

---
+++ projects.js
@@ -0,0 +1,98 @@
+const project_list = `[
+    {
+        "title" : "Audio Visualization",
+        "info"  : "This project is an audio visualizer using the P5.js framework. This was implemented by Zohiab and Rinki.",
+        "image" : "/Assets/audio_playing.jpeg",
+        "link"  : "https://github.com/ZDON-Official/Audio-Visualization"
+    },
+    {
+        "title" : "Processor Simulator",
+        "info"  : "This project is a computer simulation which uses logisim to simulate a working processor and other I/O devices, This was implemented by Zohaib and Ying.",
+        "image" : "/Assets/Processor.jpeg",
+        "link"  : "https://github.com/ZDON-Official/Processor-Simulator"
+    },
+    {
+        "title" : "MIPS Assembler",
+        "info"  : "This project converts MIPS instructions into binary to be used by a processor simulator. The project is implemented in C++.",
+        "image" : "/Assets/mips_assembler.jpeg",
+        "link"  : "https://github.com/ZDON-Official/MIPS-Assembler"
+    },
+    {
+        "title" : "Ultimate Tic-Tac-Toe",
+        "info"  : "This project is an implementation of Ultimate Tic-Tac-Toe in Python. This game utilizes the Pygame Python library to implement music and visuals.",
+        "image" : "/Assets/ultimate.jpeg",
+        "link"  : "https://github.com/ZDON-Official/UltimateTicTacToe"
+    },
+    {
+        "title" : "Spydur Research",
+        "info"  : "In this project, my team and I designed a website for finding research opportunities on campus. Our main goal was to streamline the process of finding and applying to research opportunities.",
+        "image" : "/Assets/Spydur_research.jpeg",
+        "link"  : "https://github.com/ZDON-Official/Spydur_research"
+    },
+    {
+        "title" : "Discord Invest",
+        "info"  : "Final submission for GryphHacks 2022. The goal of this project was to create a mock trading Discord bot.",
+        "image" : "/Assets/DInvestLogo.jpeg",
+        "link"  : "https://github.com/ZDON-Official/DiscordInvest"
+    },
+    {
+        "title" : "HeART of the Sea",
+        "info"  : "Final submission for AhoyHacks. Won hidden treasure award for best UI/UX. Pirates of the Caribbean inspired game made in Python using the Pygame library.",
+        "image" : "/Assets/HeartofSea.jpg",
+        "link"  : "https://github.com/ZDON-Official/AhoyHacks"
+    },
+    {
+        "title" : "ODIN Landing Page",
+        "info"  : "Implementation of a website landing page template, for ODIN Project, using HTML and CSS.",
+        "image" : "/Assets/ODIN_Landing_page.jpeg",
+        "link"  : "https://github.com/ZDON-Official/Odin-Landing-Page"
+    },
+    {
+        "title" : "Portfolio Website",
+        "info"  : "This is my personal portfolio website created using HTML, CSS, and Javascript.",
+        "image" : "/Assets/ZDON_Portfolio_website.jpeg",
+        "link"  : "https://github.com/ZDON-Official/ZDON-Official.github.io"
+    },
+    {
+        "title" : "ODIN Rock Paper Scissors",
+        "info"  : "This is an implementation of a Rock, Paper, Scissors game using HTML, CSS, and Javascript.",
+        "image" : "/Assets/ODIN-Rock-Paper-Scissor.jpeg",
+        "link"  : "https://zdon-official.github.io/Odin-Rock-Paper-Scissors/"
+    },
+    {
+        "title" : "ODIN Etch-a-Sketch",
+        "info"  : "Etch-a-Sketch for The ODIN Project. This project utilizes HTML, CSS, and Javascript.",
+        "image" : "/Assets/ODIN-Etch-a-Sketch.jpeg",
+        "link"  : "https://zdon-official.github.io/Odin-Etch-a-Sketch/"
+    }
+]`;
+
+//! Code to load projects
+
+const parent = document.getElementById("project_box");
+
+function loadResources(jsonItem) {
+  if (parent && jsonItem) {
+    const jsList = JSON.parse(jsonItem);
+    console.log(jsList);
+
+    for (i in jsList) {
+      let child = document.createElement("li");
+      child.className = "prj";
+
+      let content = `
+            <h2>${jsList[i].title}</h2>
+            <img src="${jsList[i].image}" alt="${jsList[i].title}" />
+            <div class="skills">
+                <span>C++</span>
+            </div>
+            <p>${jsList[i].info}</p>
+            <button class="prj_gtb_btn">Github</button>
+        `;
+      child.innerHTML = content;
+      parent.appendChild(child);
+    }
+  }
+}
+
+loadResources(project_list);
