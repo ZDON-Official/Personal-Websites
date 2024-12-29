b95899f772629e083db49c669bae33e2d261c1db
Zohaib <zrana1214@gmail.com>
2024-12-29 18:58:40 -0500

    new colors

--- script.js
+++ script.js
@@ -23,15 +23,36 @@ if (localStorage.getItem("dark-mode") === "dark") {
   // if the above is 'dark' then apply .dark to the body
   // document.body.classList.add("dark");
 
-  document.documentElement.style.setProperty("--background_color", "#181f28");
   document.documentElement.style.setProperty(
-    "--background_color_depth",
-    "#1d2630"
+    "--background_color",
+    "hsl(277, 40%, 13%)"
   );
-  document.documentElement.style.setProperty("--secondary_color", "#819fbf");
+  // document.documentElement.style.setProperty(
+  //   "--background_color_depth",
+  //   "#1d2630"
+  // );
+  document.documentElement.style.setProperty(
+    "--secondary_color",
+    "hsl(187, 50%, 70%)"
+  );
   document.documentElement.style.setProperty(
     "--text_color",
     "rgba(255, 255, 255, 0.87)"
+  );
+
+  document.documentElement.style.setProperty(
+    "--primary_color",
+    "hsl(278, 78%, 85%)"
+  );
+
+  document.documentElement.style.setProperty(
+    "--project_hover_text_color",
+    "#111111"
+  );
+
+  document.documentElement.style.setProperty(
+    "--box_shadow",
+    "0.1px 0.2px 2.3px rgba(0, 0, 0, 0.22), 1px 2.1px 4.9px rgba(0, 0, 0, 0.295), 2.8px 5.7px 8.9px rgba(0, 0, 0, 0.399), 4px 8px 25px rgba(0, 0, 0, 0.75)"
   );
 
   darkMode_Toggle.innerHTML = "Light Mode";
@@ -42,19 +63,41 @@ if (localStorage.getItem("dark-mode") === "dark") {
 darkMode_Toggle.addEventListener("click", () => {
   console.log("theme btn pressed");
 
+  // turn on dark mode
   if (darkMode_off === false) {
     // console.log("dark mode on");
     darkMode_off = true;
 
-    document.documentElement.style.setProperty("--background_color", "#181f28");
     document.documentElement.style.setProperty(
-      "--background_color_depth",
-      "#1d2630"
+      "--background_color",
+      "hsl(277, 40%, 13%)"
     );
-    document.documentElement.style.setProperty("--secondary_color", "#819fbf");
+    // document.documentElement.style.setProperty(
+    //   "--background_color_depth",
+    //   "#1d2630"
+    // );
+    document.documentElement.style.setProperty(
+      "--secondary_color",
+      "hsl(187, 50%, 70%)"
+    );
     document.documentElement.style.setProperty(
       "--text_color",
       "rgba(255, 255, 255, 0.87)"
+    );
+
+    document.documentElement.style.setProperty(
+      "--primary_color",
+      "hsl(278, 78%, 85%)"
+    );
+
+    document.documentElement.style.setProperty(
+      "--project_hover_text_color",
+      "#111111"
+    );
+
+    document.documentElement.style.setProperty(
+      "--box_shadow",
+      "0.1px 0.2px 2.3px rgba(0, 0, 0, 0.22), 1px 2.1px 4.9px rgba(0, 0, 0, 0.295), 2.8px 5.7px 8.9px rgba(0, 0, 0, 0.399), 4px 8px 25px rgba(0, 0, 0, 0.75)"
     );
 
     darkMode_Toggle.innerHTML = "Light Mode";
@@ -62,15 +105,36 @@ darkMode_Toggle.addEventListener("click", () => {
     localStorage.setItem("dark-mode", "dark");
     localStorage.setItem("darkMode_toggle", darkMode_off);
   } else {
-    // console.log("dark mode off");
+    // turn of dark mode
+    console.log("dark mode off");
 
-    document.documentElement.style.setProperty("--background_color", "#e9eef1");
     document.documentElement.style.setProperty(
-      "--background_color_depth",
-      "transparent"
+      "--background_color",
+      "hsl(277, 50%, 96%)"
     );
-    document.documentElement.style.setProperty("--secondary_color", "#763a3a");
+    // document.documentElement.style.setProperty(
+    //   "--background_color_depth",
+    //   "transparent"
+    // );
+    document.documentElement.style.setProperty(
+      "--secondary_color",
+      "hsl(187, 50%, 45%)"
+    );
     document.documentElement.style.setProperty("--text_color", "#333333");
+    document.documentElement.style.setProperty(
+      "--primary_color",
+      "hsl(277, 50%, 57%)"
+    );
+
+    document.documentElement.style.setProperty(
+      "--project_hover_text_color",
+      "#eeeeee"
+    );
+
+    document.documentElement.style.setProperty(
+      "--box_shadow",
+      "0 1px 1px rgba(0, 0, 0, 0.08), 0 2px 2px rgba(0, 0, 0, 0.12), 0 4px 4px rgba(0, 0, 0, 0.16), 0 8px 8px rgba(0, 0, 0, 0.2)"
+    );
 
     darkMode_Toggle.innerHTML = "Dark Mode";
 
